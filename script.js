25b69685c87b5cf3b87a0dd58b303468c7748346
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-12-01 14:20:29 -0500

    Elements object containing all necessary html elements.

---
+++ script.js
@@ -0,0 +1,9 @@
+elements = {
+  boardCont:    document.getElementById("gameBoard"),
+  newGame:      document.getElementById("newGameButton"),
+  reset:        document.getElementById("resetGameButton"),
+  score:        document.getElementById("score"),
+  speed:        document.getElementById("speed"),
+  width:        document.getElementById("width"),
+  height:       document.getElementById("height")
+};
